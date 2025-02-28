9298f63e781b9bb799a9876da888ed863e729833
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-02-27 22:54:58 -0500

    Add Scalar package

--- EosApi/Program.cs
+++ EosApi/Program.cs
@@ -1,8 +1,11 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -12,6 +15,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
