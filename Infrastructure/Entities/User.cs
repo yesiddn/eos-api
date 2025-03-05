49e2202e2d920067ac90d77fedba6082b96b532c
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-03-04 22:29:29 -0500

    Add User entity

---
+++ Infrastructure/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure;
+
+public class User
+{
+    [Key]
+    [Required]
+    public Guid Id { get; set; }
+
+    [Required]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(50)]
+    public string UserName { get; set; } = string.Empty;
+
+    [Required]
+    public string PasswordHash { get; set; } = string.Empty;
+}
