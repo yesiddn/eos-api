8b91be8caba7a77901dac5fea229d205b2880d83
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-03-06 23:13:36 -0500

    Add Priority entity

---
+++ Domain/Entities/Priority.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain;
+
+public class Priority
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    public int Value { get; set; }
+
+    [Required]
+    public string Color { get; set; } = string.Empty;
+
+    [Required]
+    public Guid UserId { get; set; }
+
+    [ForeignKey("UserId")]
+    public User User { get; set; } = new User();
+}
