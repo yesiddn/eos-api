eeff216fe8eebe207c80369800f56dd39c8a4f0e
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-03-10 21:49:10 -0500

    Add DB context

---
+++ Infrastructure/Context/EosContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Task = Domain.Task;
+
+namespace Infrastructure;
+
+public class EosContext : DbContext
+{
+    public EosContext(DbContextOptions<EosContext> options)
+        : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Auth> Auths { get; set; }
+    public DbSet<Group> Groups { get; set; }
+    public DbSet<Priority> Priorities { get; set; }
+    public DbSet<Task> Tasks { get; set; }
+}
